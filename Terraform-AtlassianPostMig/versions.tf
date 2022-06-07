d4b05bdb59f86fc3097ddd1e54d96d86883484a4
g-spencer <4820794+g-spencer@users.noreply.github.com>
2022-06-07 11:32:39 +0100

    Add files via upload

--- Terraform-AtlassianPostMig/versions.tf
+++ Terraform-AtlassianPostMig/versions.tf
@@ -1,8 +1,10 @@
 # Backend setup for Terraform remote state
 terraform {
-  required_version = ">=1.0.11"
+  required_version = ">=1.2.2"
 //  required_version = "~>0.15"   # ~ is the 'pessimistic evaluator', only allowing version increments to the minor version
 
   backend "azurerm" {
   }
 }
+
+// https://releases.hashicorp.com/terraform/
